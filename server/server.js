7b089e93b9e07ba07a8ede1e6207e6a33b924234
sticktostack <rajdeep6153@gmail.com>
2026-05-13 01:57:16 +0530

    deep isolate crash

--- server/server.js
+++ server/server.js
@@ -1,46 +1,13 @@
+console.log("A");
+
 require("dotenv").config();
 
-console.log("STEP 1");
+console.log("B");
 
 const connectDB = require("./config/db");
-console.log("STEP 2");
+
+console.log("C");
 
 const app = require("./app");
-console.log("STEP 3");
 
-const http = require("http");
-console.log("STEP 4");
-
-const { Server } = require("socket.io");
-console.log("STEP 5");
-
-connectDB();
-console.log("STEP 6");
-
-const server = http.createServer(app);
-console.log("STEP 7");
-
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST", "DELETE"]
-  }
-});
-
-console.log("STEP 8");
-
-global.io = io;
-
-io.on("connection", (socket) => {
-  console.log("New client connected");
-
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-  });
-});
-
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+console.log("D");
