6bd812664df579ea62465415b617d012c35e8cf2
sticktostack <rajdeep6153@gmail.com>
2026-05-13 02:19:09 +0530

    fresh final server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,38 @@
 require("dotenv").config();
 
-console.log("A");
+const startDeadlineReminder = require("./utils/deadlineReminder");
 
 const connectDB = require("./config/db");
-
-console.log("B");
-
 const app = require("./app");
 
-console.log("C");
+const http = require("http");
+const { Server } = require("socket.io");
 
 connectDB();
 
-console.log("D");
+// startDeadlineReminder();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "DELETE"]
+  }
+});
+
+global.io = io;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Running on ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
