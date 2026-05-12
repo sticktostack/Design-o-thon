503fd25bb11be9109ec9cd1ba9e8669207b58189
sticktostack <rajdeep6153@gmail.com>
2026-05-13 02:00:22 +0530

    working debug server

--- server/server.js
+++ server/server.js
@@ -4,10 +4,22 @@ require("dotenv").config();
 
 console.log("B");
 
-const connectDB = require("./config/db");
+const express = require("express");
 
 console.log("C");
 
-const app = require("./app");
+const app = express();
 
 console.log("D");
+
+app.get("/", (req, res) => {
+  res.send("Working");
+});
+
+console.log("E");
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Running on ${PORT}`);
+});
